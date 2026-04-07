f4235c4e41cdcf1db69568961dbb5576a064e49a
bipanabam <bipanabam147@gmail.com>
2026-04-07 16:14:58 +0545

    Update Dockerfile and entrypoint script for improved build process

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -e  # exit on error
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
+echo "Building Tailwind CSS..."
+python manage.py tailwind build
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
